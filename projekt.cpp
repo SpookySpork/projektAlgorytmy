17abe19e5c6397b1fa50fbba1d5c54863a473369
SpookySpork <ba.glad123@gmail.com>
2025-01-13 16:20:20 +0100

    dodanie algorytmu podawania liczby przez użutkownika, poprawiono rozmiary tablicy w pętlach for

--- projekt.cpp
+++ projekt.cpp
@@ -6,12 +6,23 @@ using namespace std;
 
 int *Tab;
 
-int Losuj(int n)
-{
+void Losuj(int n){
 	srand(time(NULL));
-	for(int i=0; i<=n;i++){
+	for(int i=0; i<n;i++){
 		Tab[i]=rand()%(n-1) + 1;
 		cout<<Tab[i]<<" ";
+	}
+}
+void Podaj(int n){
+	int liczba;
+	for(int i=0; i<n; i++){
+		cout<<"podaj odpowiednia liczbe(od 1 do "<<n-1<<")"<<endl;
+		cin>>liczba;
+		while(liczba>n-1){
+			cout<<"zle podana liczba, podaj jeszcze raz: "<<endl;
+			cin>>liczba;
+		}
+		Tab[i]=liczba;
 	}
 }
 
@@ -24,7 +35,11 @@ int main(){
 	cout<<"Losowac liczby?(tak/nie)"<<endl;
 	cin>>answer;
 	if(answer=="tak") Losuj(n);
-	//else Podaj();
+	else Podaj(n);
+//	for(int i=0; i<n;i++){
+//		cout<<Tab[i]<<" ";
+//	}
+	
 	
 	
 	delete [] Tab;
